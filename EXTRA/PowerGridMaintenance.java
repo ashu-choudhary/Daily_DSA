216f1397c70f3d0febfe105b91e57663a12a052a
Ashu Choudhary <ashuchoudhary7983@gmail.com>
2025-07-11 22:45:33 +0530

    remove unimportant files

---
+++ EXTRA/PowerGridMaintenance.java
@@ -0,0 +1,9 @@
+package EXTRA;
+
+public class PowerGridMaintenance {
+    public static void main(String[] args) {
+        int[][] queries = {{1,3},{2,1},{1,1},{2,2},{1,2}};
+
+        System.out.println(queries.length);
+    }
+}
